a1e0dde74c76ebc2e7d1e96c5afa22b1e9274c4a
Marco <1997mrc@gmail.com>
2019-11-10 10:25:57 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-content>
+      <Navbar />
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Navbar from "./views/Navbar";
+
+export default {
+  name: "App",
+
+  components: {
+    Navbar
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/views/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-app-bar app color="primary" dark>
+      <v-btn color="pink" dark @click.stop="drawer = !drawer">Toggle</v-btn>
+      <v-btn> </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: "Home", icon: "dashboard" },
+        { title: "About", icon: "question_answer" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
